f9b0cedbbcef0b53052babe3ef276dcc26e160a0
Roman Yeremenko <roal.yr@gmail.com>
2024-07-22 20:38:57 +0300

    Refactoring, coordinate system fix

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ buffer_limit = (height_text_main - 2) * (width_text_main - 2) - 1
 
 # CONTENT LOADING
 strings = {
-	"test_string" : "Simple Тест",
+	"main_title" : "Title",
 	"btn_text_main" : "Text",
 }
 
@@ -64,51 +64,47 @@ for c in test_text_:
 ########################################
 
 # WINDOW FUNCTIONS
-def update_win_main(stdscr, win_main):
-	win_main.resize(height_main, width_main)
-	win_main.clear()
-	win_main.attron(curses.color_pair(1))
-	win_main.border()
-	win_main.addstr(0, width_main // 2 - len(strings["test_string"]) // 2, strings["test_string"])
-	win_main.noutrefresh()
+def update_win(stdscr, win, h, w, col, title):
+	win.resize(h, w)
+	win.clear()
+	win.attron(curses.color_pair(col))
+	win.border()
+	if title:
+		win.addstr(0, width_main // 2 - len(title) // 2, title)
+	win.noutrefresh()
 	
-def update_win_tab_switcher(stdscr, win_tab_switcher):
-	win_tab_switcher.resize(height_tab_switcher, width_tab_switcher)
-	win_tab_switcher.clear()
-	win_tab_switcher.attron(curses.color_pair(2))
-	win_tab_switcher.border()
-	win_tab_switcher.noutrefresh()
-	
-def update_win_text_main(stdscr, win_text_main):
-	win_text_main.resize(height_text_main, width_text_main)
-	win_text_main.clear()
-	win_text_main.attron(curses.color_pair(2))
-	win_text_main.border()
-	win_text_main.noutrefresh()
+
 	
 # BUTTONS # TODO: make unified.
 def update_button_tab_main_text(stdscr, button_tab_main_text):
-	button_tab_main_text.resize(height_button_tab_switcher, width_button_tab_switcher)
-	button_tab_main_text.clear()
-	button_tab_main_text.attron(curses.color_pair(2))
-	if not panels["panel_text_main"].hidden():
-		button_tab_main_text.border(0, 0, 0, ' ', 0, 0, curses.ACS_VLINE, curses.ACS_LLCORNER)
-	else:
-		button_tab_main_text.border()
 	button_title = strings["btn_text_main"]
 	button_title_offset = width_button_tab_switcher // 2 - len(button_title) // 2
-	button_tab_main_text.attron(curses.A_BOLD | curses.color_pair(1))
-	button_tab_main_text.addstr(1, button_title_offset, button_title)
-	button_tab_main_text.attroff(curses.A_BOLD | curses.color_pair(1))
+	
+	button_tab_main_text.resize(height_button_tab_switcher, width_button_tab_switcher)
+	button_tab_main_text.clear()
+	
+	# Active
+	if not panels["panel_text_main"].hidden():
+		button_tab_main_text.attron(curses.color_pair(2))
+		button_tab_main_text.addstr(1, button_title_offset, button_title + '*')
+		button_tab_main_text.border(0, 0, 0, ' ', 0, 0, curses.ACS_VLINE, curses.ACS_LLCORNER)
+		button_tab_main_text.attroff(curses.color_pair(2))
+	# Inactive
+	else:
+		button_tab_main_text.attron(curses.color_pair(1))
+		button_tab_main_text.addstr(1, button_title_offset, button_title)
+		button_tab_main_text.border()
+		button_tab_main_text.attroff(curses.color_pair(1))
+	
 	button_tab_main_text.noutrefresh()
 
 # UPDATE MAIN
 def update_all(stdscr):
 	# Update windows contents and size (queue).
 	stdscr.clear()
-	update_win_main(stdscr, windows["win_main"])
-	update_win_tab_switcher(stdscr, windows["win_tab_switcher"])
-	update_win_text_main(stdscr, windows["win_text_main"])
+	update_win(stdscr, windows["win_main"], height_main, width_main, 1, strings["main_title"])
+	update_win(stdscr, windows["win_tab_switcher"], height_tab_switcher, width_tab_switcher, 1, '')
+	update_win(stdscr, windows["win_text_main"], height_text_main, width_text_main, 2, '')
 	
 	update_button_tab_main_text(stdscr, buttons["button_tab_main_text"])
 	
@@ -121,22 +117,22 @@ def update_all(stdscr):
 # UI FUNCTIONS
 def move_cursor(win_text, x, y, init_x, init_y, max_x, max_y):
 	# Contain cursor.
-	if y > max_y:
-		if x < max_x:
-			y = init_y
-			x += 1
+	if x > max_x:
+		if y < max_y:
+			x = init_x
+			y += 1
 		else:
-			y = max_y
-	if y < init_y:
-		if x > init_x:
-			y = max_y
-			x -= 1
-		else:
-			y = init_y
-	if x >= max_x:
-		x = max_x
+			x = max_x
 	if x < init_x:
-		x = init_x
+		if y > init_y:
+			x = max_x
+			y -= 1
+		else:
+			x = init_x
+	if y >= max_y:
+		y = max_y
+	if y < init_y:
+		y = init_y
 	# Move cursor.
 	try:
 		win_text.move(y,x)
@@ -250,18 +246,19 @@ def write_text(stdscr, win_text):
 			
 			
 		elif key in (curses.KEY_BACKSPACE, chr(127)):
-			index = max_y * (x-1) + (y-1)
+			index = max_x * (y-1) + (x-1)
 			if (index - 1 >= 0) and (index - 1 <= len(buffer)):
 				buffer.pop(index - 1)
 			# Update cursor position manually.
-			y -= 1
+			x -= 1
 			x, y = move_cursor(win_text, x, y, init_x, init_y, max_x, max_y)
 			# Clear the characters in the window after the buffer to prevent trailing.
 			index_last = len(buffer)
-			ix = index_last // max_y + init_x
-			iy = index_last % max_y + init_y
+			
+			ix = index_last % max_x + init_y
+			iy = index_last // max_x+ init_x
 			# Fill up with space. Restore border in right side.
-			try: win_text.addstr(ix, iy, ' '); win_text.border()
+			try: win_text.addstr(iy, ix, ' '); win_text.border()
 			except: pass
 
 		# Character insertion within limit.
@@ -272,7 +269,7 @@ def write_text(stdscr, win_text):
 				buffer.pop(-1)
 				
 			if (type(key) is str) and (len(buffer) <= buffer_limit):
-				index = max_y * (x-1) + (y-1)
+				index = max_x * (y-1) + (x-1)
 				# Insert character within existing text.
 				if index <= len(buffer):
 					buffer.insert(index, key)
@@ -284,7 +281,7 @@ def write_text(stdscr, win_text):
 						buffer.append(' ')
 					buffer.insert(index, key)
 				# Update cursor position manually.
-				y += 1
+				x += 1
 				x, y = move_cursor(win_text, x, y, init_x, init_y, max_x, max_y)
 			# TODO: add notif. when buffer limit reached.
 		
@@ -302,8 +299,8 @@ def main(stdscr):
 	# Init the screen and curses.
 	stdscr.clear()
 	curses.mousemask(curses.ALL_MOUSE_EVENTS)
-	curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+	curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+	curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 	curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
 	curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
 	curses.curs_set(False)
